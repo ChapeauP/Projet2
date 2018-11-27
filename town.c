7ba1ad0ca3ab81ee26ac943322c0b81aaf969811
Crahay <45393002+Crahay@users.noreply.github.com>
2018-11-27 14:15:48 +0100

    Add files via upload

--- town.c
+++ town.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
 
+
+typedef struct {
+	char name;
+	double x;
+	double y;
+
+}Town;
+
+Town *createTown(const char *name, double x, double y){
+	assert(name != NULL);
+	Town *createTown = town;
+	town->name = name;
+	town->x = x;
+	town->y = y;
+
+}
+
+double getTownX(Town *town){
+	return town->x;
+}
+
+double getTown
