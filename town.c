5fecd5b50ae45e6c951c81d94013e17f359ccf2b
Crahay <45393002+Crahay@users.noreply.github.com>
2018-11-27 15:01:24 +0100

    Add files via upload

--- town.c
+++ town.c
@@ -1,26 +1,48 @@
+#include "town.h"
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <stdlib.h>
 
-
-typedef struct {
-	char name;
+struct Town_t{
+	const char *name;
 	double x;
 	double y;
 
-}Town;
+};
+
+
 
 Town *createTown(const char *name, double x, double y){
 	assert(name != NULL);
-	Town *createTown = town;
+	Town *town = (Town*) malloc(sizeof(Town));
+	town->name = malloc(sizeof(name));
 	town->name = name;
 	town->x = x;
 	town->y = y;
-
+	return town;
 }
 
 double getTownX(Town *town){
 	return town->x;
 }
 
-double getTown
+double getTownY(Town *town){
+	return town->y;
+}
+
+const char *getTownName(Town *town){
+	return town->name;
+}
+
+double distanceBetweenTowns(Town *town1, Town *town2){
+	double distance;
+
+	distance = hypot((town2->x-town1->x), (town2->y-town1->y));
+	return distance;
+}
+
+void freeTown(Town *town){
+	free(&town->name);
+	free(town);
+}
