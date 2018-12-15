0e477084202434dff3a6eced0890e5e09521e2ef
Crahay <45393002+Crahay@users.noreply.github.com>
2018-12-15 14:55:06 +0100

    Add files via upload

--- tsp.c
+++ tsp.c
@@ -6,8 +6,9 @@
 #include "tsp.h"
 
 int recu_i = 0;
+//TourPosition *recu(Tour *tour, )
 
-TourPosition *recu_get_town(Tour *tour, TourPosition *tour_p, int num_town){
+/*TourPosition *recu_get_town(Tour *tour, TourPosition *tour_p, int num_town){
 	if(num_town > tour->tour_size){
 		printf("le tour ne contient pas cette position\n");
 		return NULL;
@@ -18,47 +19,59 @@ TourPosition *recu_get_town(Tour *tour, TourPosition *tour_p, int num_town){
 	}
 	return tour_p;
 }
+*/
 
 Tour *heuristic1(Tour *tour){
-	Tour *t;
+	Tour *t=createEmptyTour();
 	t->town_s = tour->town_s;
-	t->town_f = tour->town_s;
-	t->town_s->next_town = NULL;
-	t->town_f->next_town = NULL;
-	t->tour_size = 1;
-	for (int i = 0; i < tour->tour_size; ++i){
-		recu_i = 0;
-		TourPosition *Town_ref = malloc(sizeof(TourPosition));
-		Town_ref = recu_get_town(tour, tour->town_s, i);
+	t->tour_size++;
+	TourPosition *Town_ref = malloc(sizeof(TourPosition));
+	for (int i = 1; i < tour->tour_size; ++i){
+		/*recu_i = 0;
+		Town_ref = recu_get_town(tour, tour->town_s, i);*/
+		if(i == 1){
+			Town_ref = getTourStartPosition(tour);
+		}
+		Town_ref = getTourNextPosition(tour,Town_ref);
 		double d_ref;
-		int town_pos;
-		for (int j = 0; j < t->tour_size; ++j){
+		for (int j = 1; j < t->tour_size; ++j){
 			if(t->tour_size == 1){
 				t->town_s->next_town = tour->town_s->next_town;
 				d_ref = distanceBetweenTowns(t->town_s->Town,t->town_s->next_town->Town);
 			}
 			else{
 				recu_i = 0;
-				TourPosition *town_comp = recu_get_town(t,t->town_s,j);
+				TourPosition *town_comp = malloc(sizeof(TourPosition));
+				/*town_comp = recu_get_town(t,t->town_s,j);*/
+				if(j == 1){
+					town_comp = getTourStartPosition(t);
+				}
+				town_comp = getTourNextPosition(t,town_comp);
 				double dis = distanceBetweenTowns(Town_ref->Town,town_comp->Town);
+				printf("%lf\n",dis );
 				if(d_ref > dis){
 					d_ref = dis;
-					town_pos = j;
+					addTownAfterTourPosition(t, Town_ref, town_comp->Town);
 				}
 			}
 		}
-		recu_i = 0;
-		recu_get_town(t,t->town_s,town_pos)->next_town = Town_ref;
 		t->tour_size++;
+		
+		/*recu_i = 0;
+		recu_get_town(t,t->town_s,town_pos)->next_town = Town_ref;
+		*/
 	}
+	printf("%d\n", t->tour_size );
 	return t;
 }
 
-Tour *heuristic2(Tour *tour){
+/*Tour *heuristic2(Tour *tour){
 	Tour *t;
 
-}
+}*/
 
 
 int main(){
-	Tour *t = createTourFromFile("xy-belgium-towns.csv");
+	Tour *t = createTourFromFile("Doc.txt");
+	t = heuristic1(t);
+}
